e9cd7b16ba4b05a6fc801f05931bd7f14247cc7c
Casey Heinle <casey.heinle@ingage.io>
2023-02-17 15:44:54 -0800

    fleshing out the Meal List view

--- MealTracker/MealTracker/MealList.swift
+++ MealTracker/MealTracker/MealList.swift
@@ -5,4 +5,34 @@
 //  Created by Casey Heinle on 2/17/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct MealList: View {
+    let meals: [Meal]
+    var body: some View {
+        List(meals) { meal in
+            MealRow(meal: meal)
+        }
+    }
+}
+
+struct MealRow: View {
+    let meal: Meal
+    
+    var body: some View {
+        HStack {
+            Text(meal.typeOfMeal.rawValue)
+            Text(meal.mealName)
+//            Text(meal.ingredientList[0].rawValue)
+//            Text(meal.dateOfMeal.formatted(Date()))
+//            Text(meal.completedMeal.rawValue)
+            
+        }
+    }
+}
+
+struct MealList_Previews: PreviewProvider {
+    static var previews: some View {
+        MealList(meals: .exampleMeal)
+    }
+}
