713f7b00dd6e041f7e7f2e7105f73df5271719db
Casey Heinle <casey.heinle@ingage.io>
2023-02-17 12:21:27 -0800

    Adding MealType enum and dateOfMeal. Adding some notes from Martin after discussion

--- MealTracker/MealTracker/Meal.swift
+++ MealTracker/MealTracker/Meal.swift
@@ -8,11 +8,23 @@
 import Foundation
 
 // codable because I need to store this. Identifiable so I can specifically reference this meal
-struct Meals: Identifiable, Codable {
-    var id = UUID() // I want to be able to reference each unique meal
+struct Meal: Identifiable, Codable {
+    let id = UUID() // I want to be able to reference each unique meal
     
-    var typeOfMeal: String // going to be a fixed set of strings to choose from: breakfast, morning snack, lunch, afternoon snack, dinner, dessert. TODO: build the MealTypes class
+    var typeOfMeal: MealType
     var mealName: String // I want to add what the meal was, e.g. "tortilla soup", "pizza", etc.
+    let dateOfMeal: Date // when was the meal consumed?
+    // MA: decouple this thing? @same level of meal, a struct 'dish'?
     var ingredientList: [String] // I want to be able to add what ingredients were in the meal
+    // MA: perhaps a % rather than yes/no I ate all
     var completedMeal: Bool // was the meal completed, or was there food remaining on the plate
+}
+
+public enum MealType: String, Codable {
+    case Breakfast
+    case MorningSnack
+    case Lunch
+    case AfternoonSnack
+    case Dinner
+    case Dessert
 }
