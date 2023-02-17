8a0ae6665810ffa35a939568472b1c90ae2af623
Casey Heinle <casey.heinle@ingage.io>
2023-02-17 09:29:47 -0800

    initial start of the project

---
+++ MealTracker/MealTracker/MealTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealTrackerApp.swift
+//  MealTracker
+//
+//  Created by Casey Heinle on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MealTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
