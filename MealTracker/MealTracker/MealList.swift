2e0e628425dfb7005a0b44a6ab9e5284f33e66ad
Casey Heinle <casey.heinle@ingage.io>
2023-02-21 14:06:52 -0800

    Refactoring to extract a view to a new file rather than have multiple views handled in one

--- MealTracker/MealTracker/MealList.swift
+++ MealTracker/MealTracker/MealList.swift
@@ -11,31 +11,7 @@ struct MealList: View {
     let meals: [Meal]
     var body: some View {
         List(meals) { meal in
-            MealInformation(meal: meal)
-        }
-    }
-}
-
-struct MealInformation: View {
-    let meal: Meal
-    
-    var body: some View {
-        HStack {
-            Text(meal.typeOfMeal.rawValue)   
-        }
-        HStack {
-            Text(meal.mealName)
-        }
-        VStack {
-            ForEach(meal.ingredientList, id: \.self) { ingredient in
-                Text(ingredient)
-            }
-        }
-        HStack {
-            Text(formatDate(date: meal.dateOfMeal))
-        }
-        HStack {
-            Text(meal.completedMeal ? "True": "False")
+            MealRow(meal: meal)
         }
     }
 }
@@ -44,10 +20,4 @@ struct MealList_Previews: PreviewProvider {
     static var previews: some View {
         MealList(meals: .exampleMeal)
     }
-}
-
-func formatDate(date: Date) -> String {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "YYYY/MM/dd"
-    return formatter.string(from: date)
 }
---
+++ MealTracker/MealTracker/MealRow.swift
@@ -0,0 +1,35 @@
+//
+//  MealRow.swift
+//  MealTracker
+//
+//  Created by Casey Heinle on 2/21/23.
+//
+
+import SwiftUI
+
+struct MealRow: View {
+    let meal: Meal
+    
+    var body: some View {
+        VStack {
+            Text(meal.typeOfMeal.rawValue)
+            Text(meal.mealName)
+            ForEach(meal.ingredientList, id: \.self) { ingredient in
+                Text(ingredient)
+            }
+            Text(formatDate(date: meal.dateOfMeal))
+            Text(meal.completedMeal ? "True": "False")
+            
+            Button("Add New Meal", action: {
+                //let newMeal = Meal(<#T##typeOfMeal: MealType##MealType#>, mealName: <#T##String#>, dateOfMeal: <#T##Date#>, ingredientList: <#T##[String]#>, completedMeal: <#T##Bool#>)
+                //MealRow.append(newMeal)
+            })
+        }
+    }
+}
+
+func formatDate(date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "YYYY/MM/dd"
+    return formatter.string(from: date)
+}
