3c93de32d6a0b40de9153cb2bc78fcad74c7d33b
Casey Heinle <casey.heinle@ingage.io>
2023-02-22 15:15:38 -0800

    remove unused methods, commented lines, etc.

--- MealTracker/MealTracker/MealList.swift
+++ MealTracker/MealTracker/MealList.swift
@@ -9,41 +9,13 @@ import SwiftUI
 
 struct MealList: View {
     @Binding var meals: [Meal]
-//    @Binding var showAlert: Bool
-   // @Binding var mealName = ""
     
     var body: some View {
-//        ZStack {
-            List($meals) { $meal in
-                MealRow(meal: $meal)
-//                Text("Hello")
-                Button("Delete", action: {
-                    meals.removeAll(where: {$0.id == $meal.id})
-                })
-            }
-//        }
+        List($meals) { $meal in
+            MealRow(meal: $meal)
+            Button("Delete", action: {
+                meals.removeAll(where: {$0.id == $meal.id})
+            })
+        }
     }
 }
-
-func presentAlertController() {
-    let alertController = UIAlertController(title: "Add New Meal", message: nil, preferredStyle: .alert)
-    alertController.addTextField { (mealName) in
-        mealName.placeholder = "Meal Name"
-    }
-    let addAction = UIAlertAction(title: "Add", style: .default) { (action) in
-//        if let mealName = alertController.textFields?.first, let mealName = mealName.text { }
-    }
-    alertController.addAction(addAction)
-    //present(alertController, animated: true, completion: nil)
-}
-
-//    struct DemoView: View {
-//        @State var meals = [Meal].exampleMeal
-//        var body: some View {
-//            MealList(meals: $meals, showAlert: $showAlert)
-//        }
-//    }
-//    static var previews: some View {
-//        DemoView()
-//    }
-//}
