da84669b37f24752faf9102a39871e5b3c7ac2d3
Casey Heinle <casey.heinle@ingage.io>
2023-03-03 11:25:45 -0800

    change from a meal owning an array of strings to an array of ingredients

--- MealTracker/MealTracker/Meal.swift
+++ MealTracker/MealTracker/Meal.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+
 // codable because I need to store this. Identifiable so I can specifically reference this meal
 struct Meal: Identifiable, Codable, Equatable, Hashable {
     let id = UUID() // I want to be able to reference each unique meal
@@ -14,14 +15,12 @@ struct Meal: Identifiable, Codable, Equatable, Hashable {
     var typeOfMeal: MealType
     var mealName: String // I want to add what the meal was, e.g. "tortilla soup", "pizza", etc.
     var dateOfMeal: Date // when was the meal consumed?
-    // MA: decouple this thing? @same level of meal, a struct 'dish'?
-    var ingredientList: [String] // I want to be able to add what ingredients were in the meal
-    // MA: perhaps a % rather than yes/no I ate all
+    var ingredientList: [Ingredients]
     var intention: String // what was the intention during the meal? Intention examples: appreciating the different meal textures, not eating while distracted, etc.
     var completedMeal: Bool // was the meal completed, or was there food remaining on the plate
     var satiation: Double // how hungry are you after finishing the meal?
     
-    init(_ typeOfMeal: MealType, mealName: String, dateOfMeal: Date, ingredientList: [String], intention: String, completedMeal: Bool, satiation: Double) {
+    init(_ typeOfMeal: MealType, mealName: String, dateOfMeal: Date, ingredientList: [Ingredients], intention: String, completedMeal: Bool, satiation: Double) {
         self.typeOfMeal = MealType.Breakfast
         self.mealName = mealName
         self.ingredientList = ingredientList
@@ -31,8 +30,8 @@ struct Meal: Identifiable, Codable, Equatable, Hashable {
         self.satiation = satiation
     }
     
-    static let `default` = Meal(MealType.Breakfast, mealName: "oatmeal", dateOfMeal: Date(), ingredientList: ["milk", "steel cut oatmeal", "strawberries", "brown sugar", "cinnamon"], intention: "Not be on my phone while eating", completedMeal: true, satiation: 9)
-    static let `default1` = Meal(MealType.Breakfast, mealName: "eggs", dateOfMeal: Date(), ingredientList: ["milk", "steel cut oatmeal", "strawberries", "brown sugar", "cinnamon"], intention: "Taste the cinnamon and sweetness of the berries", completedMeal: true, satiation: 7)
+//    static let `default` = Meal(MealType.Breakfast, mealName: "oatmeal", dateOfMeal: Date(), ingredientList: Ingredients["milk", "steel cut oatmeal", "strawberries", "brown sugar", "cinnamon"], intention: "Not be on my phone while eating", completedMeal: true, satiation: 9)
+//    static let `default1` = Meal(MealType.Breakfast, mealName: "eggs", dateOfMeal: Date(), ingredientList: ["milk", "steel cut oatmeal", "strawberries", "brown sugar", "cinnamon"], intention: "Taste the cinnamon and sweetness of the berries", completedMeal: true, satiation: 7)
 
     static func newMeal() -> Meal {
         Meal(MealType.Breakfast, mealName: "", dateOfMeal: Date(), ingredientList: [], intention: "", completedMeal: true, satiation: 0)
@@ -40,14 +39,14 @@ struct Meal: Identifiable, Codable, Equatable, Hashable {
 }
 
 // start with an example meal for display/test purposes
-extension Array where Element == Meal {
-    static var exampleMeal: Self {
-        [
-            .default,
-            .default1,
-        ]
-    }
-}
+//extension Array where Element == Meal {
+//    static var exampleMeal: Self {
+//        [
+//            .default,
+//            .default1,
+//        ]
+//    }
+//}
 
 public enum MealType: String, Codable, CaseIterable {
     case Breakfast
